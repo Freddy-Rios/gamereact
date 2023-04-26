5a401f995d563b05a1572912bea4805d52278856
Freddy-Rios <freddyrios2000@gmail.com>
2023-04-25 23:53:59 -0600

    made changes to list Group components

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">An item</li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
+    </ul>
+  ); //testing to see if exporting is working later will change
+}
+
+export default ListGroup;
